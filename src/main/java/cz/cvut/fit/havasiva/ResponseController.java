4649d1cc8a8dcd46d2b51c2d91e359c7bebc961e
Ivan Havasi <havasiva@fit.cvut.cz>
2020-11-03 19:54:00 +0100

    Added class ResponseController

--- src/main/java/cz/cvut/fit/havasiva/ResponseController.java
+++ src/main/java/cz/cvut/fit/havasiva/ResponseController.java
@@ -1,2 +1,16 @@
-package cz.cvut.fit.havasiva;public class ResponseController {
+package cz.cvut.fit.havasiva;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResponseController {
+    public static final String template = "My response to you is: %s";
+
+    @RequestMapping("/response")
+    public TextResponse textResponse(@RequestParam(value="response", defaultValue="no response") String name) {
+        return new TextResponse(String.format(template, name));
+    }
+
 }
