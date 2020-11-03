8a40a54670387a4345893efd67ac3d45ffa4b808
Ivan Havasi <havasiva@fit.cvut.cz>
2020-11-03 19:50:25 +0100

    Added class TextResponse

---
+++ src/main/java/cz/cvut/fit/havasiva/TextResponse.java
@@ -0,0 +1,14 @@
+package cz.cvut.fit.havasiva;
+
+public class TextResponse {
+    private final String msg;
+
+    public TextResponse(String msg) {
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
+
